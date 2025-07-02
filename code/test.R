fad3ddcd2156113f8a6c05b15cb91424b4c4c9aa
Emma Cohn <ecohn@epi.org>
2025-07-02 10:32:22 -0400

    edit wages code

--- code/test.R
+++ code/test.R
@@ -12,12 +12,11 @@ cpi_data <- realtalk::c_cpi_u_annual
 cpi2024 <- cpi_data$c_cpi_u[cpi_data$year==2024]
 
 #create a data frame with wage information
-
-avg_wage <- load_cps("org", 1979:2024, orgwgt, year, age, lfstat, wage) %>%
+avg_wage <- load_cps("org", 2000:2024, orgwgt, year, age, lfstat, wage) %>%
   filter(age>=16, lfstat == 1) %>% 
   # Merge annual CPI data to data frame by year
   left_join(cpi_data, by='year') %>%
   # inflation adjust wages
   mutate(realwage = wage * (cpi2024/c_cpi_u)) %>%
-  summarize(real_avg_wage = weighted.mean(wage, w=orgwgt/12, na.rm=TRUE),
+  summarize(real_avg_wage = weighted.mean(realwage, w=orgwgt/12, na.rm=TRUE),
             .by=year)
